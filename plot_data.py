09f3d36ef9a873f4fb8deeb0705cb5008860ae8f
makerswr <hubert777@naver.com>
2019-09-16 10:19:06 +0900

    update

---
+++ plot_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open('./AnalyzedData/AngryAnalyzedData.txt') as f:
+    lines = f.readlines()
+    x = [line.split()[0] for line in lines]
+    y = [line.split()[1] for line in lines]
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+
+ax1.set_title("Plot title")
+ax1.set_xlabel('x label')
+ax1.set_ylabel('y label')
+ax1.plot(x,y,c='r', label='the data')
+
+leg = ax1.legend()
+
+plt.show()
