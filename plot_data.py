75898e57d30321327cebee7c10344b6c47dafdbc
makerswr <hubert777@naver.com>
2019-09-20 17:06:28 +0900

    plot error fix

--- plot_data.py
+++ plot_data.py
@@ -1,19 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-with open('./AnalyzedData/AngryAnalyzedData.txt') as f:
-    lines = f.readlines()
-    x = [line.split()[0] for line in lines]
-    y = [line.split()[1] for line in lines]
+a = []
+b = []
+with open('./AnalyzedData/HappyAnalyzedData.txt') as f:
+    for line in f:
+        x,y = line.split()
+        a.append(int(x))
+        b.append(int(y))
 
 fig = plt.figure()
-
 ax1 = fig.add_subplot(111)
-
 ax1.set_title("Plot title")
 ax1.set_xlabel('x label')
 ax1.set_ylabel('y label')
-ax1.plot(x,y)
+ax1.scatter(a,b)
+plt.gca().invert_yaxis()
 
 leg = ax1.legend()
 
