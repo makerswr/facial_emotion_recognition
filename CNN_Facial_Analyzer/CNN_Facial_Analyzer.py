53725c729639f9b2f4327c25159ba6ee1ac98bf8
Have To Freeze <54876203+Have-To-Freeze@users.noreply.github.com>
2019-09-23 20:18:07 +0900

    Add files via upload

---
+++ CNN_Facial_Analyzer/CNN_Facial_Analyzer.py
@@ -0,0 +1,18 @@
+import CNN_Model
+import CNN_Dataset
+
+data = CNN_Dataset.DATA()
+model = CNN_Model.CNN(data.inputShape, data.numOfClass)
+
+if __name__ == "__main__":
+    batchSize = 128
+    epochs = int(input('Epochs: '))
+
+    model.fit(data.x_train, data.y_train,
+              batch_size = batchSize,
+              epochs = epochs,
+              validation_split = 0.2)
+              
+    score = model.evaluate(data.x_train, data.y_train)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
