e775a1081777d3680e70f264f02f572c35c4180f
Have To Freeze <asesdsx@gmail.com>
2019-09-24 21:48:41 +0900

    Add files via upload

--- CNN_Facial_Analyzer/CNN_Main.py
+++ CNN_Facial_Analyzer/CNN_Main.py
@@ -1,5 +1,6 @@
 import CNN_Model
 import CNN_Dataset
+from skeras import plot_acc, plot_loss
 from keras.models import load_model
 
 data = CNN_Dataset.DATA()
@@ -9,7 +10,7 @@ if __name__ == "__main__":
     batchSize = 128
     epochs = int(input('Epochs: '))
 
-    model.fit(data.x_train, data.y_train,
+    history = model.fit(data.x_train, data.y_train,
               batch_size = batchSize,
               epochs = epochs,
               validation_split = 0.2)
@@ -17,5 +18,10 @@ if __name__ == "__main__":
     score = model.evaluate(data.x_train, data.y_train)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+
+    plot_loss(history)
+    plt.show()
+    plot_acc(history)
+    plt.show()
 
     model.save('CNN_Facial_Sensitivity_Analyzer.h5')
