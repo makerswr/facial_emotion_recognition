67ae8cf53baa9c066dd1a75150b8b559462ff930
Have To Freeze <asesdsx@gmail.com>
2019-10-07 09:59:39 +0900

    Add files via upload

---
+++ CNN_Model.py
@@ -0,0 +1,24 @@
+import keras as K
+from keras import layers, models
+
+class Model(models.Sequential):
+    def __init__(self, numOfClass, inputShape):
+        super().__init__()
+
+        self.add(layers.Conv2D(32, kernel_size = (3, 3),
+                                   activation = 'relu',
+                                   input_shape = inputShape))
+        self.add(layers.Conv2D(64, kernel_size = (3, 3), 
+                                   activation = 'relu'))
+        self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+        self.add(layers.Dropout(0.25))
+        self.add(layers.Flatten())
+        self.add(layers.Dense(128, activation = 'relu'))
+        self.add(layers.Dropout(0.5))
+        # self.add(layers.Dense(256, activation = 'relu'))
+        # self.add(layers.Dropout(0.5))
+        self.add(layers.Dense(numOfClass, activation = 'softmax'))
+
+        self.compile(loss = K.losses.categorical_crossentropy,
+                     optimizer = 'rmsprop',
+                     metrics = ['accuracy'])
