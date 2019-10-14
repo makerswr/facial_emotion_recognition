5473134dfbdc9dd2fcfd952d0b64fa1e8dab2638
Wharang Shin <hubert777@naver.com>
2019-10-14 11:45:30 +0900

    Update CNN_Model_Object.py
    adam lr = 0.00001

--- CNN_Facial_Analyzer/CNN_Model_Object.py
+++ CNN_Facial_Analyzer/CNN_Model_Object.py
@@ -15,73 +15,73 @@ class Model(models.Sequential):
         self.add(layers.Conv2D(32, kernel_size = (3, 3), 
                                    activation = 'relu'))                          
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
-        self.add(layers.Dropout(0.5))
+        self.add(layers.Dropout(0.25))
 
         # 2D convolution layer 2
         self.add(layers.Conv2D(64, kernel_size = (3, 3),
-                                   activation = 'relu',)
+                                   activation = 'relu',))
         self.add(layers.Conv2D(64, kernel_size = (3, 3), 
                                    activation = 'relu'))
         self.add(layers.Conv2D(64, kernel_size = (3, 3), 
                                    activation = 'relu'))                           
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
-        self.add(layers.Dropout(0.5))
+        self.add(layers.Dropout(0.25))
 
         # 2D convolution layer 3
         self.add(layers.Conv2D(128, kernel_size = (3, 3),
-                                   activation = 'relu',)
+                                   activation = 'relu',))
         self.add(layers.Conv2D(128, kernel_size = (3, 3), 
                                    activation = 'relu'))
         self.add(layers.Conv2D(128, kernel_size = (3, 3), 
                                    activation = 'relu'))                           
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
-        self.add(layers.Dropout(0.5))
+        self.add(layers.Dropout(0.25))
 
         # 2D convolution layer 4
         self.add(layers.Conv2D(256, kernel_size = (3, 3),
-                                   activation = 'relu',)
+                                   activation = 'relu',))
         self.add(layers.Conv2D(256, kernel_size = (3, 3), 
                                    activation = 'relu'))
         self.add(layers.Conv2D(256, kernel_size = (3, 3), 
                                    activation = 'relu'))                           
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
-        self.add(layers.Dropout(0.5))
+        self.add(layers.Dropout(0.25))
 
         # 2D convolution layer 5
         self.add(layers.Conv2D(512, kernel_size = (3, 3),
-                                   activation = 'relu',)
+                                   activation = 'relu',))
         self.add(layers.Conv2D(512, kernel_size = (3, 3), 
                                    activation = 'relu'))
         self.add(layers.Conv2D(512, kernel_size = (3, 3), 
                                    activation = 'relu'))                           
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
-        self.add(layers.Dropout(0.5))
+        self.add(layers.Dropout(0.2))
 
         self.add(layers.Flatten())
 
         # Dense Layer 1
         self.add(layers.Dense(4096, activation = 'relu'))
         self.add(layers.BatchNormalization())
-        self.add(layers.Dropout(0.5))
+        self.add(layers.Dropout(0.2))
 
         # Dense Layer 2
-        self.add(layers.Dense(4096), activation = 'relu'))
+        self.add(layers.Dense(4096), activation = 'relu')
         self.add(layers.BatchNormalization())
-        self.add(layers.Dropout(0.4))
+        self.add(layers.Dropout(0.15))
 
         # Dense Layer 3
         self.add(layers.Dense(4096, activation = 'relu'))
         self.add(layers.BatchNormalization())
-        self.add(layers.Dropout(0.3))
+        self.add(layers.Dropout(0.15))
 
         # Dense Layer 4
         self.add(layers.Dense(4096, activation = 'relu'))
         self.add(layers.BatchNormalization())
-        self.add(layers.Dropout(0.2))
+        self.add(layers.Dropout(0.1))
 
         # Dense Output layer
         self.add(layers.Dense(numOfClass, activation = 'softmax'))
 
         self.compile(loss = K.losses.categorical_crossentropy,
-                     optimizer = 'adam',
+                     optimizer = adam(learning_rate=0.00001, beta_1=0.85, beta_2=0.998, amsgrad=False),
                      metrics = ['accuracy'])
