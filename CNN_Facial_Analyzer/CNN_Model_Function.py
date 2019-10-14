9e62ad50004fb7f92b51037c6835d060427628a2
Have To Freeze <asesdsx@gmail.com>
2019-10-14 10:37:53 +0900

    Add files via upload

--- CNN_Facial_Analyzer/CNN_Model_Function.py
+++ CNN_Facial_Analyzer/CNN_Model_Function.py
@@ -2,44 +2,80 @@ import keras as K
 from keras import layers, models
 
 def Model(inputShape, numOfClass):
+    # 2D convolution layer 1
     self.add(layers.Conv2D(32, kernel_size = (3, 3),
                                activation = 'relu',
                                input_shape = inputShape))
-    self.add(layers.BatchNormalization())
     self.add(layers.Conv2D(32, kernel_size = (3, 3), 
                                activation = 'relu'))
-    self.add(layers.BatchNormalization())
+    self.add(layers.Conv2D(32, kernel_size = (3, 3), 
+                               activation = 'relu'))                          
     self.add(layers.MaxPooling2D(pool_size = (2, 2)))
     self.add(layers.Dropout(0.5))
 
+    # 2D convolution layer 2
     self.add(layers.Conv2D(64, kernel_size = (3, 3),
-                               activation = 'relu',
-                               input_shape = inputShape))
-    self.add(layers.BatchNormalization())
+                               activation = 'relu',)
     self.add(layers.Conv2D(64, kernel_size = (3, 3), 
                                activation = 'relu'))
-    self.add(layers.BatchNormalization())
+    self.add(layers.Conv2D(64, kernel_size = (3, 3), 
+                               activation = 'relu'))                           
+    self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+    self.add(layers.Dropout(0.5))
+    
+    # 2D convolution layer 3
+    self.add(layers.Conv2D(128, kernel_size = (3, 3),
+                               activation = 'relu',)
+    self.add(layers.Conv2D(128, kernel_size = (3, 3), 
+                          activation = 'relu'))
+    self.add(layers.Conv2D(128, kernel_size = (3, 3), 
+                               activation = 'relu'))                           
+    self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+    self.add(layers.Dropout(0.5))
+
+    # 2D convolution layer 4
+    self.add(layers.Conv2D(256, kernel_size = (3, 3),
+                               activation = 'relu',)
+    self.add(layers.Conv2D(256, kernel_size = (3, 3), 
+                               activation = 'relu'))
+    self.add(layers.Conv2D(256, kernel_size = (3, 3), 
+                               activation = 'relu'))                           
+    self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+    self.add(layers.Dropout(0.5))
+
+    # 2D convolution layer 5
+    self.add(layers.Conv2D(512, kernel_size = (3, 3),
+                               activation = 'relu',)
+    self.add(layers.Conv2D(512, kernel_size = (3, 3), 
+                               activation = 'relu'))
+    self.add(layers.Conv2D(512, kernel_size = (3, 3), 
+                               activation = 'relu'))                           
     self.add(layers.MaxPooling2D(pool_size = (2, 2)))
     self.add(layers.Dropout(0.5))
     
     self.add(layers.Flatten())
 
+    # Dense Layer 1
     self.add(layers.Dense(128, activation = 'relu'))
     self.add(layers.BatchNormalization())
     self.add(layers.Dropout(0.5))
-
+   
+    # Dense Layer 2
     self.add(layers.Dense(256, activation = 'relu'))
     self.add(layers.BatchNormalization())
     self.add(layers.Dropout(0.25))
 
+    # Dense Layer 3
     self.add(layers.Dense(512, activation = 'relu'))
     self.add(layers.BatchNormalization())
     self.add(layers.Dropout(0.125))
 
+    # Dense Layer 4
     self.add(layers.Dense(1024, activation = 'relu'))
     self.add(layers.BatchNormalization())
     self.add(layers.Dropout(0.1))
 
+    # Dense Output layer
     self.add(layers.Dense(numOfClass, activation = 'softmax'))
 
     self.compile(loss = K.losses.categorical_crossentropy,
--- CNN_Facial_Analyzer/CNN_Model_Object.py
+++ CNN_Facial_Analyzer/CNN_Model_Object.py
@@ -6,45 +6,80 @@ from keras.layers import Dense, Dropout, BatchNormalization, Flatten
 class Model(models.Sequential):
     def __init__(self, inputShape, numOfClass):
         super().__init__()
-
+        # 2D convolution layer 1
         self.add(layers.Conv2D(32, kernel_size = (3, 3),
                                    activation = 'relu',
                                    input_shape = inputShape))
-        self.add(layers.BatchNormalization())
         self.add(layers.Conv2D(32, kernel_size = (3, 3), 
                                    activation = 'relu'))
-        self.add(layers.BatchNormalization())
+        self.add(layers.Conv2D(32, kernel_size = (3, 3), 
+                                   activation = 'relu'))                          
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
         self.add(layers.Dropout(0.5))
 
+        # 2D convolution layer 2
         self.add(layers.Conv2D(64, kernel_size = (3, 3),
-                                   activation = 'relu',
-                                   input_shape = inputShape))
-        self.add(layers.BatchNormalization())
+                                   activation = 'relu',)
         self.add(layers.Conv2D(64, kernel_size = (3, 3), 
                                    activation = 'relu'))
-        self.add(layers.BatchNormalization())
+        self.add(layers.Conv2D(64, kernel_size = (3, 3), 
+                                   activation = 'relu'))                           
+        self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+        self.add(layers.Dropout(0.5))
+
+        # 2D convolution layer 3
+        self.add(layers.Conv2D(128, kernel_size = (3, 3),
+                                   activation = 'relu',)
+        self.add(layers.Conv2D(128, kernel_size = (3, 3), 
+                                   activation = 'relu'))
+        self.add(layers.Conv2D(128, kernel_size = (3, 3), 
+                                   activation = 'relu'))                           
+        self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+        self.add(layers.Dropout(0.5))
+
+        # 2D convolution layer 4
+        self.add(layers.Conv2D(256, kernel_size = (3, 3),
+                                   activation = 'relu',)
+        self.add(layers.Conv2D(256, kernel_size = (3, 3), 
+                                   activation = 'relu'))
+        self.add(layers.Conv2D(256, kernel_size = (3, 3), 
+                                   activation = 'relu'))                           
+        self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+        self.add(layers.Dropout(0.5))
+
+        # 2D convolution layer 5
+        self.add(layers.Conv2D(512, kernel_size = (3, 3),
+                                   activation = 'relu',)
+        self.add(layers.Conv2D(512, kernel_size = (3, 3), 
+                                   activation = 'relu'))
+        self.add(layers.Conv2D(512, kernel_size = (3, 3), 
+                                   activation = 'relu'))                           
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
         self.add(layers.Dropout(0.5))
 
         self.add(layers.Flatten())
 
+        # Dense Layer 1
         self.add(layers.Dense(128, activation = 'relu'))
         self.add(layers.BatchNormalization())
         self.add(layers.Dropout(0.5))
 
+        # Dense Layer 2
         self.add(layers.Dense(256, activation = 'relu'))
         self.add(layers.BatchNormalization())
-        self.add(layers.Dropout(0.25))
+        self.add(layers.Dropout(0.4))
 
+        # Dense Layer 3
         self.add(layers.Dense(512, activation = 'relu'))
         self.add(layers.BatchNormalization())
-        self.add(layers.Dropout(0.125))
+        self.add(layers.Dropout(0.3))
 
+        # Dense Layer 4
         self.add(layers.Dense(1024, activation = 'relu'))
         self.add(layers.BatchNormalization())
-        self.add(layers.Dropout(0.1))
+        self.add(layers.Dropout(0.2))
 
+        # Dense Output layer
         self.add(layers.Dense(numOfClass, activation = 'softmax'))
 
         self.compile(loss = K.losses.categorical_crossentropy,
