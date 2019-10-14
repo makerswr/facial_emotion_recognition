1939887c106bd26e5340aeef365e7b3f6bcaf4ef
Have To Freeze <asesdsx@gmail.com>
2019-10-14 09:45:36 +0900

    Add files via upload

--- CNN_Facial_Analyzer/CNN_Model_Function.py
+++ CNN_Facial_Analyzer/CNN_Model_Function.py
@@ -2,24 +2,48 @@ import keras as K
 from keras import layers, models
 
 def Model(inputShape, numOfClass):
-    model = models.Sequential(inputShape, numOfClass)
+    self.add(layers.Conv2D(32, kernel_size = (3, 3),
+                               activation = 'relu',
+                               input_shape = inputShape))
+    self.add(layers.BatchNormalization())
+    self.add(layers.Conv2D(32, kernel_size = (3, 3), 
+                               activation = 'relu'))
+    self.add(layers.BatchNormalization())
+    self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+    self.add(layers.Dropout(0.5))
 
-    model.add(layers.Conv2D(32, kernel_size = (3, 3),
-                                activation = 'relu',
-                                input_shape = inputShape))
-    model.add(layers.Conv2D(64, kernel_size = (3, 3), 
-                                activation = 'relu'))
-    model.add(layers.MaxPooling2D(pool_size = (2, 2)))
-    model.add(layers.Dropout(0.25))
-    model.add(layers.Flatten())
-    model.add(layers.Dense(128, activation = 'relu'))
-    model.add(layers.Dropout(0.5))
-    # model.add(layers.Dense(256, activation = 'relu'))
-    # model.add(layers.Dropout(0.3))
-    model.add(layers.Dense(numOfClass, activation = 'softmax'))
+    self.add(layers.Conv2D(64, kernel_size = (3, 3),
+                               activation = 'relu',
+                               input_shape = inputShape))
+    self.add(layers.BatchNormalization())
+    self.add(layers.Conv2D(64, kernel_size = (3, 3), 
+                               activation = 'relu'))
+    self.add(layers.BatchNormalization())
+    self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+    self.add(layers.Dropout(0.5))
+    
+    self.add(layers.Flatten())
 
-    model.compile(loss = K.losses.categorical_crossentropy,
-                  optimizer = 'rmsprop',
-                  metrics = ['accuracy'])
+    self.add(layers.Dense(128, activation = 'relu'))
+    self.add(layers.BatchNormalization())
+    self.add(layers.Dropout(0.5))
+
+    self.add(layers.Dense(256, activation = 'relu'))
+    self.add(layers.BatchNormalization())
+    self.add(layers.Dropout(0.25))
+
+    self.add(layers.Dense(512, activation = 'relu'))
+    self.add(layers.BatchNormalization())
+    self.add(layers.Dropout(0.125))
+
+    self.add(layers.Dense(1024, activation = 'relu'))
+    self.add(layers.BatchNormalization())
+    self.add(layers.Dropout(0.1))
+
+    self.add(layers.Dense(numOfClass, activation = 'softmax'))
+
+    self.compile(loss = K.losses.categorical_crossentropy,
+                 optimizer = 'adam',
+                 metrics = ['accuracy'])
 
     return model
--- CNN_Facial_Analyzer/CNN_Model_Object.py
+++ CNN_Facial_Analyzer/CNN_Model_Object.py
@@ -1,5 +1,7 @@
 import keras as K
-from keras import layers, models
+from keras import models
+from keras.layers import Conv2D, Maxpooling2D
+from keras.layers import Dense, Dropout, BatchNormalization, Flatten
 
 class Model(models.Sequential):
     def __init__(self, inputShape, numOfClass):
@@ -8,17 +10,43 @@ class Model(models.Sequential):
         self.add(layers.Conv2D(32, kernel_size = (3, 3),
                                    activation = 'relu',
                                    input_shape = inputShape))
+        self.add(layers.BatchNormalization())
+        self.add(layers.Conv2D(32, kernel_size = (3, 3), 
+                                   activation = 'relu'))
+        self.add(layers.BatchNormalization())
+        self.add(layers.MaxPooling2D(pool_size = (2, 2)))
+        self.add(layers.Dropout(0.5))
+
+        self.add(layers.Conv2D(64, kernel_size = (3, 3),
+                                   activation = 'relu',
+                                   input_shape = inputShape))
+        self.add(layers.BatchNormalization())
         self.add(layers.Conv2D(64, kernel_size = (3, 3), 
                                    activation = 'relu'))
+        self.add(layers.BatchNormalization())
         self.add(layers.MaxPooling2D(pool_size = (2, 2)))
-        self.add(layers.Dropout(0.25))
-        self.add(layers.Flatten())
-        self.add(layers.Dense(128, activation = 'relu'))
         self.add(layers.Dropout(0.5))
-        # model.add(layers.Dense(256, activation = 'relu'))
-        # model.add(layers.Dropout(0.3))
+
+        self.add(layers.Flatten())
+
+        self.add(layers.Dense(128, activation = 'relu'))
+        self.add(layers.BatchNormalization())
+        self.add(layers.Dropout(0.5))
+
+        self.add(layers.Dense(256, activation = 'relu'))
+        self.add(layers.BatchNormalization())
+        self.add(layers.Dropout(0.25))
+
+        self.add(layers.Dense(512, activation = 'relu'))
+        self.add(layers.BatchNormalization())
+        self.add(layers.Dropout(0.125))
+
+        self.add(layers.Dense(1024, activation = 'relu'))
+        self.add(layers.BatchNormalization())
+        self.add(layers.Dropout(0.1))
+
         self.add(layers.Dense(numOfClass, activation = 'softmax'))
 
         self.compile(loss = K.losses.categorical_crossentropy,
-                     optimizer = 'rmsprop',
+                     optimizer = 'adam',
                      metrics = ['accuracy'])
